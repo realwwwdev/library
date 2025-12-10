298f3e130578b22fc654567625282c3ba7536c41
i.saetgareev <i.saetgareev@bftcom.com>
2025-12-10 16:24:13 +0300

    Изменен сценарий демонстрации работы приложения

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,23 +1,21 @@
 package app;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Library library = new Library();
-        library.addBook("Анна Каренина", "Толстой", 10724);
-        library.addBook("Война и мир", "Толстой", 10725);
-        library.addBook("Мастер и Маргарита", "Булгаков", 10581);
-        library.addBook("Собачье сердце", "Булгаков", 10582);
-        library.showBooks();
+        library.addBook("Анна Каренина", "Толстой", 10724, Arrays.asList("Роман", "Социальный"));
+        library.addBook("Война и мир", "Толстой", 10725, Arrays.asList("Роман", "Исторический"));
+        library.addBook("Мастер и Маргарита", "Булгаков", 10581, Arrays.asList("Роман", "Философский"));
+        library.addBook("Собачье сердце", "Булгаков", 10582, Arrays.asList("Повесть", "Социальный"));
 
         library.changeTitle(10725, "Война и мир [2 издание]");
         library.showBooks();
 
-        library.deleteBook(10724);
-        library.showBooks();
-
-        System.out.println("\nПоиск книг по слову 'мир':");
-        library.findBookByTitle("мир");
         System.out.println("\nПоиск книг по автору 'Булгаков':");
         library.findBooksByAuthor(("булгаков"));
+
+        library.deleteBook(10724);
     }
 }
